8040c0b766b2b0c4eb63fc5f7f1cb62779a5c693
Robin Westhaver <robinwesthaver@gmail.com>
2019-05-06 20:21:31 +0000

    completed exercise

---
+++ index.js
@@ -0,0 +1,18 @@
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')             
+       .on('error', function (err) {                                   
+         throw err; 
+       })
+       .on('response', function (response) {       
+         console.log('Downloading image...');                   
+         console.log("Response Status Code: " + response.statusCode);
+         console.log("Response content: " + response.headers['content-type']);
+       })
+       .on('end', function () {
+        console.log('Download complete.'); 
+       })
+       .pipe(fs.createWriteStream('./future.jpg'));
+
+       
